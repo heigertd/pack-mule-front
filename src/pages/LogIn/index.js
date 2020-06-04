864ac0d0f19bfecf14bebe51e6d488b6b81cb6be
Devin Heigert <heigertd@gmail.com>
2020-06-04 15:51:20 -0700

    login age styling

--- src/pages/LogIn/index.js
+++ src/pages/LogIn/index.js
@@ -34,6 +34,10 @@ export default function LogIn() {
         })
     }
 
+    function handleNewAccount(){
+        history.push("/signup")
+    }
+
     const readSessions =()=>{
         API.readSessions().then(res =>{
             console.log(res.data)
@@ -41,23 +45,26 @@ export default function LogIn() {
     }
 
     return (
-        <div>
-            <div>
-                <form>
-                    <div className='login-input-div'>
-                        <lable for = 'username'>UserName</lable>
-                        <input name = 'username' type = 'text' onChange = {handleInputChange}></input>
-                    </div>
-                    <div className='login-input-div'>
-                        <lable for = 'password'>Password</lable>
-                        <input name = 'password' type = 'password' onChange = {handleInputChange}></input>
-                    </div>
-                    <button onClick = {handleLogIn}>LogIn</button>
-                </form>
-                <button onClick = {readSessions}>check log in</button>
+        <div className = 'login-render-div'>
+            <div className = 'login-header'>
+                <h1>Pack Mule</h1>
             </div>
-            <div>
-                <p>Need an account? <a href= '/signup'>Sign Up here!</a></p>
+            <div className = 'login-page'>
+                    <div className = 'login-form-div'>
+                        <form className = 'login-form'>
+                            <div className='login-input-div'>
+                                {/* <lable for = 'username'>UserName</lable> */}
+                                <input className = 'login-input-field' name = 'username' type = 'text' placeholder='UserName' onChange = {handleInputChange}></input>
+                            </div>
+                            <div className='login-input-div'>
+                                {/* <lable for = 'password'>Password</lable> */}
+                                <input className = 'login-input-field' name = 'password' type = 'password' placeholder = 'Password' onChange = {handleInputChange}></input>
+                            </div>
+                            <button className = 'login-button' onClick = {handleLogIn}>LogIn</button>
+                            <button className = 'login-button' onClick = {handleNewAccount}>Create an Account</button>
+                        </form>
+                        {/* <button onClick = {readSessions}>check log in</button> */}
+                    </div> 
             </div>
         </div>
     )
