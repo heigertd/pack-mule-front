2457307e5ed4d891ad7cc95ca2941f157e83f24b
Devin Heigert <heigertd@gmail.com>
2020-06-16 17:12:31 -0700

    set to deploy

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,39 +1,41 @@
 import axios from 'axios';
+const BASE_URL = 'http://localhost:8080';
+const BASE_URL = 'https://pack-mule-api.herokuapp.com';
 
 export default {
     getAllHikers: function(){
-       return axios.get('http://localhost:8080/api/hikers', {withCredentials: true})
+       return axios.get(`${BASE_URL}/api/hikers`, {withCredentials: true})
     },
 
     createNewHiker: function(newHiker){
-        return axios.post('http://localhost:8080/api/hikers', newHiker)
+        return axios.post(`${BASE_URL}/api/hikers`, newHiker)
     },
 
     logInHiker: function(logInInput){
-        console.log('input', logInInput)
-        return axios.post('http://localhost:8080/login', logInInput, {withCredentials: true})
+        // console.log('input', logInInput)
+        return axios.post(`${BASE_URL}/login`, logInInput, {withCredentials: true})
     },
 
     deleteHiker: function(userId){
-        console.log('input', userId)
-        return axios.delete(`http://localhost:8080/deletehiker/${userId}`, {withCredentials: true})
+        // console.log('input', userId)
+        return axios.delete(`${BASE_URL}/deletehiker/${userId}`, {withCredentials: true})
     },
 
     getClickedHiker: function(hikerId){
-        console.log(hikerId)
-        return axios.get(`http://localhost:8080/specifichiker/${hikerId}`, {withCredentials: true})
+        // console.log(hikerId)
+        return axios.get(`${BASE_URL}/specifichiker/${hikerId}`, {withCredentials: true})
     },
 
     readSessions: function(){
-        return axios.get('http://localhost:8080/readsessions', {withCredentials: true})
+        return axios.get(`${BASE_URL}/readsessions`, {withCredentials: true})
     },
 
     updateUserInfo: function(newInfo, userId){
-        console.log(newInfo, userId)
-        return axios.put(`http://localhost:8080/updatehiker/${userId}`, newInfo, {withCredentials: true})
+        // console.log(newInfo, userId)
+        return axios.put(`${BASE_URL}/updatehiker/${userId}`, newInfo, {withCredentials: true})
     },
 
     signOut: function(){
-        return axios.get(`http://localhost:8080/signout`, {withCredentials: true})
+        return axios.get(`${BASE_URL}/signout`, {withCredentials: true})
     }
 }
