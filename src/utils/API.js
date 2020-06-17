284a149f8cd62c4cc586e40fba61897194164e33
Devin Heigert <heigertd@gmail.com>
2020-06-16 17:27:51 -0700

    wip

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = 'http://localhost:8080';
+// const BASE_URL = 'http://localhost:8080';
 const BASE_URL = 'https://pack-mule-api.herokuapp.com';
 
 export default {
